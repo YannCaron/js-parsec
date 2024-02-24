06f98dbb71843d92bfc80d3b8fd07bb7597407cc
Yann Caron <cyann74@gmail.com>
2024-02-24 08:36:04 +0100

    fix configuration to ignore dist from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    modulePathIgnorePatterns: ['dist/'],
+    coveragePathIgnorePatterns : ['dist/']  
   };
