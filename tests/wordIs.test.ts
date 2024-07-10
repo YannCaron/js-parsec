44fa20108532af484a2734d93d80ae41946f1d22
Yann Caron <cyann74@gmail.com>
2024-07-10 18:46:46 +0200

    change case

--- tests/wordIs.test.ts
+++ tests/wordIs.test.ts
@@ -10,13 +10,13 @@ describe('basic wordIs', () => {
 
     const parser = sequence(wordIs("hi"), charIn(' '), wordIs('world'))
 
-    test('wordis should parse "hi world"', () => {
+    test('wordIs should parse "hi world"', () => {
         const ctx = new Context("hi world")
         expect(parser.parse(ctx)).toBe(Result.Parsed)
         expect(ctx.pos).toBe(8)
     })
 
-    test('wordis should not parse "hi wurld"', () => {
+    test('wordIs should not parse "hi wurld"', () => {
         const ctx = new Context("hi wurld")
         expect(parser.parse(ctx)).toBe(Result.NotParsed);
         expect(ctx.pos).toBe(0)
@@ -24,7 +24,7 @@ describe('basic wordIs', () => {
         expect(ctx.stack).toStrictEqual([])
     })
     
-    test('wordis to string', () => {
+    test('wordIs to string', () => {
         expect(parser.toString()).toBe(`("hi" [ ] "world")`)
     })
 
